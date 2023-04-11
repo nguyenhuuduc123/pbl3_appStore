75d56e448a6f4d14fa0eaefd44758402eee98e5b
nguyen huu duc <duccccnguyen@gmail.com>
2023-04-11 19:55:28 +0700

    Duclatao

--- DAL/AppStoreService.cs
+++ DAL/AppStoreService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace pbl3_appstore.services
 {
@@ -16,17 +17,53 @@ namespace pbl3_appstore.services
         public AppStoreService() { }
 
 
-        public bool getPersonCurrent(Person p)
+        public Person login(String password,string username)
         {
-            if (p.role == "admin" ) {
-            return true}
+            Person? person = (from p in appStoreDbcontext._instance.persons
+                             where p.email == username && p.password == password
+                             select p).FirstOrDefault();
+           
 
 
-            return false;
+
+
+            return person!;
         }
 
-        
-        
+        public List<Person> getAllPerson()
+        {
+            List<Person> persons = from p in appStoreDbcontext._instance.persons.ToList();
+;        return persons;
+        }
+        public Person getPersonByEmail(string email)
+        {
+            Person? person = (from p in appStoreDbcontext._instance.persons
+                             where p.email == email
+                             select p).FirstOrDefault();
+            return person!;
+        }
+
+        public Person getPersonByRole(string rolename)
+        {
+            Person? person = (from p in appStoreDbcontext._instance.persons
+                              where p.role.name_role == rolename
+                              select p).FirstOrDefault();
+
+           return person!;
+        }
+
+        public string roleNamePersonLogin(string name,string password)
+        {
+            Person person = login(password, name);
+            return person.role.name_role;
+        }
+        public List<Item> getItemByCategory(bool desending,Category c)
+        {
+
+
+            return new List<Item>();
+        }
+               
        
     }
 }
--- DTO/appStoreDbcontext.cs
+++ DTO/appStoreDbcontext.cs
@@ -10,17 +10,17 @@ namespace pbl3_appstore.models
 {
     public class appStoreDbcontext : DbContext
     {
-      // private static appStoreDbcontext instance  = new appStoreDbcontext();
+      private static appStoreDbcontext instance  = new appStoreDbcontext();
 
-       // public static appStoreDbcontext _instance
-       // {
-       //    get { return instance; }
-      //     
-      // }
+        public static appStoreDbcontext _instance
+        {
+           get { return instance; }
+           
+       }
         
-       // private appStoreDbcontext() {
-      //      Console.WriteLine(ConfigurationManager.AppSettings["connectionString"]);
-      // }
+       private appStoreDbcontext() {
+            Console.WriteLine(ConfigurationManager.AppSettings["connectionString"]);
+       }
         //private readonly string connectionStrings = @"Data Source=localhost,1433;Initial Catalog=appdb1;User ID=SA;Password=password123;Trusted_Connection=False;Encrypt=False;MultipleActiveResultSets=true;";
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
